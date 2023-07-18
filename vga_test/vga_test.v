977d213005e059b61c0fb3852a6d7588f96d1b59
Chaitanya Katpatal <17334965+chai-kat@users.noreply.github.com>
2023-07-18 14:43:41 +0200

    change focus of project to first output just red to a display. add hsync structure

--- vga_test/vga_test.v
+++ vga_test/vga_test.v
@@ -1,11 +1,8 @@
 // VGA module for the DE10-Lite board
-// takes RGB input from and outputs to VGA with HSync and VSync
+// outputs red to 800x600 VGA with HSync and VSync
 
 // first 3 are inputs from the framebuffer
 module vga_test(
-    input [3:0] R,
-    input [3:0] G,
-    input [3:0] B,
     input MAX10_CLK1_50,
     output VGA_B[3:0],
     output VGA_R[3:0],
@@ -13,3 +10,33 @@ module vga_test(
     output VGA_HS,
     output VGA_VS, 
 );
+
+// there are 1040 pixels in a line, so we keep track of where we are
+reg [10:0] hsync_counter;
+
+// there are 666 lines in the whole frame
+reg [9:0] vsync_counter;
+
+initial begin 
+	hsync_counter <= 11'b00000000000;
+	vsync_counter <= 10'b0000000000;
+end
+
+// for this one, HSYNC and VSYNC are both POSITIVE
+always @(posedge MAX10_CLK1_50) begin
+	// front porch of 56 pixels
+	if ((hsync_counter >= 0) or (hsync_counter <= 56)) begin
+	
+	end
+	
+	// hsync pulse of 120 pixels
+	else if ((hsync_counter >= 57) or (hsync_counter <= 177)) begin
+	
+	end
+	
+	// back porch of 64 pixels
+	
+	//video of 800 pixels
+	
+	hsync_counter = hsync_counter + 1;
+end
